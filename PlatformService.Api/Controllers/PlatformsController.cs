e69880799cf3ae83f3fbd963b03bc3f8391c2d49
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-17 13:57:18 +0300

    Defined the basic code for the Platforms controller

---
+++ PlatformService.Api/Controllers/PlatformsController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlatformService.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PlatformsController : ControllerBase
+{
+
+}
