ebe49ace1f063d08bf129f441d8f3e72dbf01dc4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-17 12:44:05 +0300

    Defined the IPlatformRepo interface

---
+++ PlatformService.Api/Data/IPlatformRepo.cs
@@ -0,0 +1,12 @@
+using PlatformService.Api.Models;
+
+namespace PlatformService.Api.Data;
+
+
+public interface IPlatformRepo
+{
+    bool SaveChanges();
+    IEnumerable<Platform> GetAllPlatforms();
+    Platform GetPlatformById(int id);
+    void CreatePlatform(Platform platform);
+}
