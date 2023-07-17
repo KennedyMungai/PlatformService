a0dfb46c9da326ced2026395f2bbe6731f883a7a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-17 13:12:04 +0300

    Defined the class which is to seed the data

--- PlatformService.Api/Data/PrepDb.cs
+++ PlatformService.Api/Data/PrepDb.cs
@@ -1,9 +1,31 @@
+using PlatformService.Api.Models;
+
 namespace PlatformService.Api.Data;
 
 public static class PrepDb
 {
     public static void PrepPopulation(IApplicationBuilder app)
     {
+        using (var serviceScope = app.ApplicationServices.CreateScope())
+        {
+            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+        }
+    }
 
+    private static void SeedData(AppDbContext context)
+    {
+        if (!context.Platforms.Any())
+        {
+            Console.WriteLine(" ---> Seeding Data");
+            context.Platforms.AddRange(
+                new Platform() { Name = "Dot Net", Publisher = "Microsoft", Cost = "Free" },
+                new Platform() { Name = "MySql", Publisher = "Oracle", Cost = "Free" },
+                new Platform() { Name = "Linux", Publisher = "Linux Foundation", Cost = "Free" }
+            );
+        }
+        else
+        {
+            Console.WriteLine(" --> We already have data");
+        }
     }
 }
