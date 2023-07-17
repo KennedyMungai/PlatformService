683716c0ec7d87237d6fb22a14bd27141cc6abae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-17 13:14:51 +0300

    Simplified the using statement fir teh service scope

--- PlatformService.Api/Data/PrepDb.cs
+++ PlatformService.Api/Data/PrepDb.cs
@@ -6,10 +6,8 @@ public static class PrepDb
 {
     public static void PrepPopulation(IApplicationBuilder app)
     {
-        using (var serviceScope = app.ApplicationServices.CreateScope())
-        {
-            SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
-        }
+        using var serviceScope = app.ApplicationServices.CreateScope();
+        SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
     }
 
     private static void SeedData(AppDbContext context)
