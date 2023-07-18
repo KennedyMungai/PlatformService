bc632ff29c220e2a8e7e8a87384d2f57a74d150a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-18 12:40:10 +0300

    Wrote a controller for the application

---
+++ CommandsService.Api/Controllers/PlatformsController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Api.Controllers;
+
+[ApiController]
+[Route("api/commands/[controller]")]
+public class PlatformsController : ControllerBase
+{
+    public PlatformsController()
+    {
+
+    }
+
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+        Console.WriteLine("--> Inbound Post # Command Service");
+    }
+}
