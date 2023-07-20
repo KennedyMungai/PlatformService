c478f8a32626ad21006453834a4def1fba3fe862
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-20 11:58:12 +0300

    Added automatter to the builder services

--- CommandsService.Api/Program.cs
+++ CommandsService.Api/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
