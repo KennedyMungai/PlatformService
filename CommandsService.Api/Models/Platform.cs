d510900e81ae50437252aa550088e6ae13b620ca
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-20 11:02:15 +0300

    INstantiated the Commmands inside the Platform model file

--- CommandsService.Api/Models/Platform.cs
+++ CommandsService.Api/Models/Platform.cs
@@ -6,5 +6,5 @@ public class Platform
     public int Id { get; set; }
     public int ExternalId { get; set; }
     public string Name { get; set; } = string.Empty;
-    public ICollection<Command> Commands { get; set; }
+    public ICollection<Command> Commands { get; set; } = new List<Command>();
 }
