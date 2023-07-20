28f0547de19ea42cc7e958940bb5f25f87adfcad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-20 11:24:59 +0300

    Added some dependency injection for the DbCOntext

--- CommandsService.Api/Data/CommandRepo.cs
+++ CommandsService.Api/Data/CommandRepo.cs
@@ -5,6 +5,13 @@ namespace CommandsService.Api.Data;
 
 public class CommandRepo : ICommandRepo
 {
+    private readonly AppDbContext _context;
+
+    public CommandRepo(AppDbContext context)
+    {
+        _context = context;
+    }
+
     public void CreateCommand(int platformId, Command command)
     {
         throw new NotImplementedException();
