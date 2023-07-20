a2ac0fa54e2e6d5d70a926f3b0760013e7b9146a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-20 10:56:27 +0300

    Defined the Platform Model file

---
+++ CommandsService.Api/Models/Platform.cs
@@ -0,0 +1,9 @@
+namespace CommandsService.Api.Models;
+
+
+public class Platform
+{
+    public int Id { get; set; }
+    public int ExternalId { get; set; }
+    public string Name { get; set; } = string.Empty;
+}
