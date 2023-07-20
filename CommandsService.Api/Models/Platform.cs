362c03e70737203e92c844bfaa4df78609393067
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-20 11:03:52 +0300

    Added data annotations to the model file

--- CommandsService.Api/Models/Platform.cs
+++ CommandsService.Api/Models/Platform.cs
@@ -1,10 +1,16 @@
 namespace CommandsService.Api.Models;
 
+using System.ComponentModel.DataAnnotations;
+
 
 public class Platform
 {
+    [Key]
+    [Required]
     public int Id { get; set; }
+    [Required]
     public int ExternalId { get; set; }
+    [Required]
     public string Name { get; set; } = string.Empty;
     public ICollection<Command> Commands { get; set; } = new List<Command>();
 }
