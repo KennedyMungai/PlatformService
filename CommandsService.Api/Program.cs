2d7bd0b33f4576fa1800cdf958b62f7ac884e1c5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-20 11:10:33 +0300

    Added the DbContext to the application

--- CommandsService.Api/Program.cs
+++ CommandsService.Api/Program.cs
@@ -1,3 +1,6 @@
+using CommandsService.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("InMemoryDb"));
 
 var app = builder.Build();
 
