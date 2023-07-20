79ffae2fa624d616ec3e0b85f3c8153b0ce9f5b5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-20 11:01:25 +0300

    Added a reference to the Collection of Command objects inside the PLatform model

--- CommandsService.Api/Models/Platform.cs
+++ CommandsService.Api/Models/Platform.cs
@@ -6,4 +6,5 @@ public class Platform
     public int Id { get; set; }
     public int ExternalId { get; set; }
     public string Name { get; set; } = string.Empty;
+    public ICollection<Command> Commands { get; set; }
 }
