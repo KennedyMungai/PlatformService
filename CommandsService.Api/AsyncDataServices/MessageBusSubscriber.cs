d8f6bfdf4ccb1a23f83135824fcee3eba697fc40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-21 11:34:59 +0300

    Wrote some more logic for the InitializeRabbitMQ method, and added the RabbitMQ_ConnectionShutDown and an overrode of the Dispose method from BackgroundService

--- CommandsService.Api/AsyncDataServices/MessageBusSubscriber.cs
+++ CommandsService.Api/AsyncDataServices/MessageBusSubscriber.cs
@@ -9,6 +9,7 @@ public class MessageBusSubscriber : BackgroundService
     private readonly IEventProcessor _eventProcessor;
     private IConnection _connection;
     private IModel _channel;
+    private string _queueName;
 
     public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
     {
@@ -26,10 +27,33 @@ public class MessageBusSubscriber : BackgroundService
 
         _connection = factory.CreateConnection();
         _channel = _connection.CreateModel();
+        _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+        var _queueName = _channel.QueueDeclare().QueueName;
+        _channel.QueueBind(queue: _queueName, exchange: "trigger", routingKey: "");
+
+        Console.WriteLine("--> Listening on the Message Bus...");
+
+        _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
     }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         throw new NotImplementedException();
+    }
+
+    private void RabbitMQ_ConnectionShutdown(object? sender, ShutdownEventArgs e)
+    {
+        Console.WriteLine("--> Connection Shutdown");
+    }
+
+    public override void Dispose()
+    {
+        if (_channel.IsOpen)
+        {
+            _channel.Close();
+            _connection.Close();
+        }
+
+        base.Dispose();
     }
 }
