a0fa7988ebeef0e2c0ef98dc9d28c3a76d9093a3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-21 10:07:55 +0300

    Created the PlatformPublishedDto

---
+++ CommandsService.Api/Models/Dto/PlatformPublishedDto.cs
@@ -0,0 +1,8 @@
+namespace CommandsService.Api.Models.Dto;
+
+public class PlatformPublishedDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Event { get; set; } = string.Empty;
+}
