d7e2ff0e540ce69a78233e49e6c43177f9b6612b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-21 11:21:08 +0300

    Added some dependency injection for the MessageBusSubscriber class

--- CommandsService.Api/AsyncDataServices/MessageBusSubscriber.cs
+++ CommandsService.Api/AsyncDataServices/MessageBusSubscriber.cs
@@ -1,7 +1,18 @@
+using CommandsService.Api.EventProcessing;
+
 namespace CommandsService.Api.AsyncDataServices;
 
 public class MessageBusSubscriber : BackgroundService
 {
+    private readonly IConfiguration _configuration;
+    private readonly IEventProcessor _eventProcessor;
+
+    public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
+    {
+        _configuration = configuration;
+        _eventProcessor = eventProcessor;
+    }
+
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
         throw new NotImplementedException();
