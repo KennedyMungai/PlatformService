76bb7c2512db70dcc085a9bcb3f64de18c463024
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-21 11:25:11 +0300

    Wrote the function to initialize RabbitMQ

--- CommandsService.Api/AsyncDataServices/MessageBusSubscriber.cs
+++ CommandsService.Api/AsyncDataServices/MessageBusSubscriber.cs
@@ -1,4 +1,5 @@
 using CommandsService.Api.EventProcessing;
+using RabbitMQ.Client;
 
 namespace CommandsService.Api.AsyncDataServices;
 
@@ -6,11 +7,25 @@ public class MessageBusSubscriber : BackgroundService
 {
     private readonly IConfiguration _configuration;
     private readonly IEventProcessor _eventProcessor;
+    private IConnection _connection;
+    private IModel _channel;
 
     public MessageBusSubscriber(IConfiguration configuration, IEventProcessor eventProcessor)
     {
         _configuration = configuration;
         _eventProcessor = eventProcessor;
+    }
+
+    private void InitializeRabbitMQ()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = _configuration["RabbitMQHost"],
+            Port = int.Parse(_configuration["RabbitMQPort"])
+        };
+
+        _connection = factory.CreateConnection();
+        _channel = _connection.CreateModel();
     }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
