dca47ab90ac093368401358d0003248c489f5931
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-21 10:42:36 +0300

    Added a switch statement for dynamic return statements basoed on the event type

--- CommandsService.Api/EventProcessing/EventProcessor.cs
+++ CommandsService.Api/EventProcessing/EventProcessor.cs
@@ -1,4 +1,6 @@
+using System.Text.Json;
 using AutoMapper;
+using CommandsService.Api.Models.Dto;
 
 namespace CommandsService.Api.EventProcessing;
 
@@ -17,6 +19,23 @@ public class EventProcessor : IEventProcessor
     public void ProcessEvent(string message)
     {
         throw new NotImplementedException();
+    }
+
+    private EventType DetermineEvent(string notificationMessage)
+    {
+        Console.WriteLine("--> Determining event");
+
+        var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+
+        switch (eventType.Event)
+        {
+            case "Platform_Published":
+                Console.WriteLine("--> Platform Published Event Detected");
+                return EventType.PlatformPublished;
+            default:
+                Console.WriteLine("--> Could not determine the event type");
+                return EventType.Undetermined;
+        }
     }
 }
 
