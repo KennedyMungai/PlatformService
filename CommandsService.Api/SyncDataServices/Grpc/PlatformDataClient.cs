acf3b2b301115d05160136b617299ec3d5da493e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-21 15:15:37 +0300

    Added the logic to the ReturnAllPlatforms method

--- CommandsService.Api/SyncDataServices/Grpc/PlatformDataClient.cs
+++ CommandsService.Api/SyncDataServices/Grpc/PlatformDataClient.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using CommandsService.Api.Models;
+using Grpc.Net.Client;
+using PlatformService.Api;
 
 namespace CommandsService.Api.SyncDataServices.Grpc;
 
@@ -17,6 +19,20 @@ public class PlatformDataClient : IPlatformDataClient
 
     public IEnumerable<Platform> ReturnAllPlatforms()
     {
-        throw new NotImplementedException();
+        Console.WriteLine($" --> Calling GRPC Service {_configuration["GrpcPlatform"]}");
+        var channel = GrpcChannel.ForAddress(_configuration["GrpcPlatform"]);
+        var client = new GrpcPlatform.GrpcPlatformClient(channel);
+        var request = new GetAllRequest();
+
+        try
+        {
+            var reply = client.GetAllPlatforms(request);
+            return _mapper.Map<IEnumerable<Platform>>(reply.Platform);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"--> Could not call GRPC Server {ex.Message}");
+            return null;
+        }
     }
 }
