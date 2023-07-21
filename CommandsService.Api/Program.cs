e53ef50a04e6bb0ff7a24cfec94a4f024a437790
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-07-21 15:19:22 +0300

    Added the PlatformDataClient services and gave it a lifetime of scoped

--- CommandsService.Api/Program.cs
+++ CommandsService.Api/Program.cs
@@ -1,6 +1,7 @@
 using CommandsService.Api.AsyncDataServices;
 using CommandsService.Api.Data;
 using CommandsService.Api.EventProcessing;
+using CommandsService.Api.SyncDataServices.Grpc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,6 +16,7 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDataba
 builder.Services.AddScoped<ICommandRepo, CommandRepo>();
 builder.Services.AddSingleton<IEventProcessor, EventProcessor>();
 builder.Services.AddHostedService<MessageBusSubscriber>();
+builder.Services.AddScoped<IPlatformDataClient, PlatformDataClient>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
